69a9ed70e33a4dab11122ca635e75a8ef3e797c2
syktox <markus.kammerstetter@hotmail.com>
2026-04-16 19:32:50 +0200

    Fixed a sizing problem

--- macos/Runner/MainFlutterWindow.swift
+++ macos/Runner/MainFlutterWindow.swift
@@ -2,10 +2,15 @@ import Cocoa
 import FlutterMacOS
 
 class MainFlutterWindow: NSWindow {
+  private let minimumContentSize = NSSize(width: 480, height: 760)
+
   override func awakeFromNib() {
     let flutterViewController = FlutterViewController()
-    let windowFrame = self.frame
+    var windowFrame = self.frame
     self.contentViewController = flutterViewController
+    self.contentMinSize = minimumContentSize
+    windowFrame.size.width = max(windowFrame.size.width, minimumContentSize.width)
+    windowFrame.size.height = max(windowFrame.size.height, minimumContentSize.height)
     self.setFrame(windowFrame, display: true)
 
     RegisterGeneratedPlugins(registry: flutterViewController)
